e3f69e44c90940a2f81fdb08aaca8e4a47761bd8
Toolka Web Solution <haseeb@outlook.in>
2024-11-02 02:34:40 +0530

    commit

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 var config = {} 
 
-
 // your telegram API credentials
 // please go to https://my.telegram.org to create your API credentials
 config.telegramApiId = 123456789;
-config.telegramApiHash = '';
-
+config.telegramApiHash = 'PLACEHOLDER_HASH';
 
 // server settings
 config.hostname = '127.0.0.1';
 config.port = 3003;
-
 
 module.exports = config;
